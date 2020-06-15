9e28548b28bc0cc9d8641e3ee93562d5e45f3392
王孟宇 <wangmengyu04@maoyan.com>
2020-06-15 11:11:01 +0800

    add .

---
+++ 739.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+
+
+
+"""
+根据每日 气温 列表，请重新生成一个列表，
+对应位置的输出是需要再等待多久温度才会升高超过该日的天数。如果之后都不会升高，请在该位置用 0 来代替。
+
+例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+
+提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+"""
+
+
+class Solution(object):
+
+    def dailyTemperatures(self, T):
+        """
+        维护一个递减栈，后入栈的元素总比栈顶元素小
+        对比当前元素与栈顶元素的大小：
+        1，当前元素 < 栈顶元素：入栈
+        2，当前元素 > 栈顶元素：弹出栈顶元素，记录下两者下标即为所求天数
+        * 用栈记录的是T的下标
+        """
+        stack = list()
+        t_length = len(T)
+        res_list = [0 for _ in range(t_length)]
+        for key, value in enumerate(T):
+            while stack and T[stack[-1]] < value:
+                res_list[stack[-1]] = key - stack[-1]
+                stack.pop()
+            stack.append(key)
+        return res_list
+
+
+
+
+
+def test():
+    t = [73, 74, 75, 71, 69, 72, 76, 73]
+    print Solution().dailyTemperatures(t)
+
+
+if __name__ == '__main__':
+    test()
--- test.py
+++ test.py
@@ -1,77 +1,17 @@
 # coding: utf8
-from collections import defaultdict
-
-origin_data = [
-                {'appkey': 'xxxx', 'http': 'xxx', "date": "2012-10-10"},
-                {'appkey': 'xxxx', 'http': 'xxx', "date": "2012-10-11"},
-                {'appkey': 'xxxx', 'http': 'xxx', "date": "2012-10-12"},
-                {'appkey': 'xxxx', 'http': 'xxx', "date": "2012-10-13"},
-                {'appkey': 'xxxx', 'http': 'xxx', "date": "2012-10-14"},
-
-                {'appkey': 'xxxx1', 'http': 'xxx1', "date": "2012-10-15"},
-                {'appkey': 'xxxx1', 'http': 'xxx1', "date": "2012-10-16"},
-                {'appkey': 'xxxx1', 'http': 'xxx1', "date": "2012-10-17"},
-                {'appkey': 'xxxx1', 'http': 'xxx1', "date": "2012-10-18"},
-                {'appkey': 'xxxx1', 'http': 'xxx1', "date": "2012-10-19"},
-
-                {'appkey': 'xxxx2', 'http': 'xxx1', "date": "2012-10-20"},
-                {'appkey': 'xxxx2', 'http': 'xxx1', "date": "2012-10-21"},
-                {'appkey': 'xxxx2', 'http': 'xxx1', "date": "2012-10-22"},
-                {'appkey': 'xxxx2', 'http': 'xxx1', "date": "2012-10-23"},
-                {'appkey': 'xxxx2', 'http': 'xxx1', "date": "2012-10-24"},
-                {'appkey': 'xxxx2', 'http': 'xxx1', "date": "2012-10-25"},
-                {'appkey': 'xxxx2', 'http': 'xxx1', "date": "2012-10-26"},
-                {'appkey': 'xxxx2', 'http': 'xxx1', "date": "2012-10-27"},
-
-                {'appkey': 'xxxx3', 'http': 'xxx1', "date": "2012-10-28"},
-                {'appkey': 'xxxx3', 'http': 'xxx1', "date": "2012-10-29"},
-                {'appkey': 'xxxx3', 'http': 'xxx1', "date": "2012-10-30"},
-                {'appkey': 'xxxx3', 'http': 'xxx1', "date": "2012-10-31"},
-                {'appkey': 'xxxx3', 'http': 'xxx1', "date": "2012-10-32"},
-                {'appkey': 'xxxx3', 'http': 'xxx1', "date": "2012-10-33"},
-
-                {'appkey': 'xxxx4', 'http': 'xxx1', "date": "2012-11-29"},
-                {'appkey': 'xxxx4', 'http': 'xxx1', "date": "2012-11-30"},
-                {'appkey': 'xxxx4', 'http': 'xxx1', "date": "2012-11-31"},
-                {'appkey': 'xxxx4', 'http': 'xxx1', "date": "2012-11-32"},
-                {'appkey': 'xxxx4', 'http': 'xxx1', "date": "2012-11-33"},
-]
-
-target = {
-    "http可用": lambda i: i+1,
-    "http调用量": lambda i: i+1,
-    "响应时间": lambda i: i+1,
-    "机器数": lambda i: i+1,
-    "qps": lambda i: i+1,
-    "资源利用": lambda i: i+1,
-}
 
 
-def main():
-    dic = dict()
-    render_data = []
-    i = 0
-    try:
-        for d in origin_data:
-            appkey = d["appkey"]
-            if dic.has_key(appkey) and dic.get("target") == target[i]:
-                dic[appkey]["detail"][d["date"]] = 10
-            else:
-                dic[appkey] = dict()
-                dic[appkey].update(d)
-                dic[appkey].pop("date")
-                dic[appkey]["target"] = i
-                dic[appkey]["detail"] = dict()
-                dic[appkey]["detail"][d["date"]] = 20
-            i += 1
-    except IndexError:
-        pass
-    for k, v in dic.iteritems():
-        d = dict()
-        d.update(v)
-        render_data.append(d)
-    return render_data
 
 
-if __name__ == '__main__':
-    print main()
+
+
+a = [1,2]
+b = [2,3]
+a.append(b)
+b.append(a)
+print a
+print b
+
+del a
+del b
+
