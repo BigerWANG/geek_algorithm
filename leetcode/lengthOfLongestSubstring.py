8a5c8cad1efd29ef5e1518bafa31554bdbf274ea
wangmengyu <wangmengyu@yupiao.com>
2020-07-18 22:34:03 +0800

    update

--- leetcode/lengthOfLongestSubstring.py
+++ leetcode/lengthOfLongestSubstring.py
@@ -22,3 +22,20 @@ class Solution(object):
         维护一个队列，按下表往队列里添加字符串，并且查看这个窗口里有没有重复的值，有的话就出队
         查看有没有重复值的方法是设置一个set
         """
+        if not s:
+            return
+        left = 0  # 左侧的index
+        lookup = set()  # 设置一个set保存重复元素
+        n = len(s)
+        max_len, curr_len = 0, 0
+        for i in range(n):
+            curr_len += 1
+            while s[i] in lookup:
+                lookup.remove(s[left])  # 如果当前元素在set中，移除set中的当前元素
+                left += 1
+                curr_len -= 1
+            if curr_len > max_len:
+                max_len = curr_len
+            lookup.add(s[i])
+        return max_len
+
