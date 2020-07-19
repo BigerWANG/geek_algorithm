2e6be9d0f790ca1f11f5d87d20263a99a929c3ce
wangmengyu <m15901428525@163.com>
2020-07-19 17:03:58 +0800

    add.

---
+++ leetcode/merge_array.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+"""
+合并两个有序数组
+给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+
+说明:
+
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+
+示例:
+
+输入:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+输出: [1,2,2,3,5,6]
+"""
+
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        num_copy = nums1[:m]
+        nums1[:] = []
+        p1, p2 = 0, 0
+        while p1 < m and p2 < n:
+            if num_copy[p1] < nums2[p2]:
+                nums1.append(num_copy[p1])
+                p1 += 1
+            else:
+                nums1.append(nums2[p2])
+                p2 += 1
+        if p1 < m:
+            nums1[p1+p2:] = nums1[p1:]
+        if p2 < n:
+            nums1[p1+p2:] = nums2[p2:]
+
+
+
+
+
+def test():
+    s = Solution()
+    nums1 = [1,2,3,0,0,0]
+    m = 3
+    nums2 = [2,5,6]
+    n = 3
+    s.merge(nums1, m, nums2, n)
+    print nums1
+
+
+if __name__ == '__main__':
+    test()
+
