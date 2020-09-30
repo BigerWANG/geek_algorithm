65215299e9f51bcfd9109262638fb57b0bbdec2a
wangmengyu <wangmengyu@yupiao.com>
2020-09-30 20:41:49 +0800

    add DP

---
+++ leetcode/DP/fib.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+
+def feb(n):
+    """
+    斐波那契数列 DP 写法
+    :param num:
+    :return:
+    """
+    from collections import defaultdict
+    d = defaultdict(int)
+    if n <= 0:
+        return 0
+    d[0] = 0
+    d[1] = 1
+    for i in range(2, n+1):
+        d[i] = d[i - 1] + d[i - 2]
+    return d[n] % 1000000007
+
+class Solution:
+    def uniquePaths(self, m, n):
+        dp = []
+        # 先生成一个左右边界都为 1 的二维数组
+        dp = [[1]*n] + [[1]+[0] * (n-1) for _ in range(m-1)]
+        print dp
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
+
+
+
+
+
+
+
+
+print Solution().uniquePaths(4, 3)
+print Solution1().uniquePaths(4, 3)
---
+++ leetcode/DP/maxSubArray.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+
+"""
+52.最大子序和
+
+"""
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        动态规划：
+        当上一个位置的数字大于0时，把当前位置数与上一个位置数相加，并更新到当前位置
+        """
+        n = len(nums)
+
+        for i in range(1, n):
+            if nums[i-1] > 0:
+                nums[i] += nums[i-1]
+        print nums
+        return max(nums)
+
+
+def max_sub_array(nums):
+    """
+    算法
+    定义两个变量
+    sum: 当前连续最大子数组的和
+    ans: 返回结果
+    如果sum > 0，则说明sum对结果有增益效果，则保留结果并加上当前遍历数字
+    如果sum <= 0 ，则说明sum对结果无增益效果，需要舍弃，则sum直接更新为当前遍历数字
+    每次比较sum和ans的大小，将最大值设置为ans, 遍历完成返回结果
+    """
+    ans = nums[0]  # 当前值
+    _sum = 0  # 当前连续子数组的值
+    n = len(nums)
+    for i in range(n):
+        if _sum > 0:
+            _sum += nums[i]
+        else:
+            _sum = nums[i]
+        ans = max(_sum, ans)
+
+    return ans
+
+
+
+
+nums = [-2, 1, -3, 4, -1, 2]
+# nums = [1]
+
+# print max_sub_array(nums)
+print Solution().maxSubArray(nums)
