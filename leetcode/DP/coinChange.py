0abe6df7eb7b7a7b2004c83c11a26be30b033039
王孟宇 <wangmengyu04@maoyan.com>
2020-10-10 15:23:57 +0800

    add

--- leetcode/DP/coinChange.py
+++ leetcode/DP/coinChange.py
@@ -29,6 +29,39 @@ class Solution(object):
 
 
 
+class Solution1(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        算法：
+        1, 转换成子问题
+            each_amount: 从0开始递增的amount：for eache_amount in range(0, amount+1)
+            最后剩下硬币为1：f(each_amount) = min(f(each_amount - 2)+1, f(each_amount-5)+1)
+            最后剩下硬币为2：f(each_amount) = min(f(each_amount - 1)+1, f(each_amount-5)+1)
+            最后剩下硬币为5：f(each_amount) = min(f(each_amount - 1)+1, f(each_amount-2)+1)
+            。。。
+
+        2，状态转移方程：f(each_amount) = min(f(each_amount - 1)+1, f(each_amount-2)+1, f(each_amount-5)+1)
+        3, 初始条件和边界情况：
+            判断coins数组是否为空
+            因为要求最小值，所以dp数组中的初始化值应为float('INF')
+            如果 amount = 0，则结果也是0: dp[0] = 0
+            dp 数组为长度为 amount + 1，防止越界
+        """
+        if not coins:
+            return -1
+        dp = [float('INF') for _ in range(amount+1)]
+        dp[0] = 0
+        for coin in coins:
+            for each_amount in range(coin, amount+1):  # 要找的是x比coin大的情况
+                dp[each_amount] = min(dp[each_amount-coin]+1, dp[each_amount])
+
+        return dp[amount] if dp[amount] != float('INF') else -1
+
+
+
 def coin_change(coins, amount):
     """
     递归写法
@@ -54,7 +87,7 @@ def coin_change(coins, amount):
     return recsion(coins, amount)
 
 
-coins = [2,]
-amount = 3
+coins = [1,2,5]
+amount = 11
 # print(coin_change(coins, amount))
-print(Solution().coinChange(coins, 3))
+print(Solution().coinChange(coins, amount))
--- leetcode/DP/uniquePaths.py
+++ leetcode/DP/uniquePaths.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 
 
+
+
+
 class Solution(object):
     @staticmethod
     def uniquePaths(m, n):
@@ -9,20 +12,23 @@ class Solution(object):
         :type n: int
         :rtype: int
         算法：
-        1，生成一个m*n的网格，网格的左边界和右边界为1
-        2，dp公式：dp[i][j] = dp[i+1][j]+dp[i][j+1]
-        3, 返回最大值即 dp[i][j]
+        dp表中每个格子都保存着从起始点到当前的步数
+        1， 最小子问题: 假设只有两步可以走到target dp[m][n], 所以 dp[m][n] = dp[m-1][n] + dp[m][n-1]
+        2, 状态转移方程： dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        3, 初始值和边界条件：
+            1）因为只能往下走或者向右走，所以左边界和上边界的值都是1
+            2）防止数组越界，循环起始点是1
         """
         if m == 0 or n == 0:
             return 0
         dp = [[0 for _ in range(n)] for _ in range(m)]
 
-        # 因为只能往下走或者向右走，所以
+        # 因为只能往下走或者向右走，所以左边界和上边界的值都是1
         dp[0] = [1 for _ in range(n)]
         for i in dp:
             i[0] = 1
 
-        for i in range(1, m):
+        for i in range(1, m): # 从第一个网格开始走
             for j in range(1, n):
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
         return dp[-1][-1]
